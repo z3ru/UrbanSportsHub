5fe436638451021896bb3a046f6c442db356c26a
Eric Mewes <mewes.e@gmail.com>
2025-03-07 16:02:06 +0100

    Add link to GitHub

--- src/pages/VenuesPage/VenuesPage.jsx
+++ src/pages/VenuesPage/VenuesPage.jsx
@@ -5,10 +5,14 @@ const VenuesPage = () => {
     <>
       <h1 className="venues__headline">Disclaimer</h1>
       <div className="venues">
-        This project is for educational and demonstration purposes only. It is
-        intended to showcase a security vulnerability in the Urban Sports Club
-        check-in process and suggest improvements. It should not be used for
-        fraudulent activities.
+        <p>
+          This project is for educational and demonstration purposes only. It is
+          intended to showcase a security vulnerability in the Urban Sports Club
+          check-in process. It should not be used for fraudulent activities.
+        </p>
+        <a href="https://github.com/ericelric/UrbanSportsHub">
+          Read more on GitHub
+        </a>
       </div>
     </>
   );
